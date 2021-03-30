e83919c45ad1539d0a1e24084e403a84af7a04a2
jannkasper <jann.kasper@gmail.com>
2021-03-30 07:36:20 +0200

    truffle | test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+var CryptoZombies = artifacts.require("./CryptoZombies.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(CryptoZombies);
 };
