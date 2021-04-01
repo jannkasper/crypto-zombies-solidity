8648b9a583f3ad38897bdcfe621b57a4d636ccb4
jannkasper <jann.kasper@gmail.com>
2021-04-01 05:19:12 +0200

    deploy | test

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,60 @@
+require('dotenv').config()
+
+// Initialize HDWalletProvider
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+// Initialize LoomTruffleProvider
+// const LoomTruffleProvider = require('loom-truffle-provider');
+
+// Set your own mnemonic here
+const privateKey = process.env.METAMASK_PRIVATE_KEY;
+
+// Module exports to make this configuration available to Truffle itself
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-
-  networks: {
-   development: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   },
-   test: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   }
-  }
-
+    // Object with configuration for each network
+    networks: {
+        // Configuration for mainnet
+        mainnet: {
+            provider: function () {
+                // Setting the provider with the Infura Rinkeby address and Token
+                return new HDWalletProvider(privateKey, "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY)
+            },
+            network_id: "1"
+        },
+        // Configuration for rinkeby network
+        rinkeby: {
+            // Special function to setup the provider
+            provider: function () {
+                // Setting the provider with the Infura Rinkeby address and Token
+                return new HDWalletProvider(privateKey, "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY)
+            },
+            // Network id is 4 for Rinkeby
+            network_id: 4
+        },
+        // Configuration for ropsten network
+        ropsten: {
+            // Special function to setup the provider
+            provider: function () {
+                // Setting the provider with the Infura Rinkeby address and Token
+                return new HDWalletProvider(privateKey, "https://ropsten.infura.io/v3/" + process.env.INFURA_KEY)
+            },
+            // Network id is 4 for Ropsten
+            network_id: 3
+        },
+        // loom_testnet: {
+        //     provider: function() {
+        //         const privateKey = process.env.LOOM_PRIVATE_KEY;
+        //         const chainId = 'extdev-plasma-us1';
+        //         const writeUrl = 'http://extdev-plasma-us1.dappchains.com:80/rpc';
+        //         const readUrl = 'http://extdev-plasma-us1.dappchains.com:80/query';
+        //         return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+        //     },
+        //     network_id: '9545242630824'
+        // },
+        test: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        }
+    }
 };
